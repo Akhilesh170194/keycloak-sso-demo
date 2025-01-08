f6ea54024efa0ec4d784a7385d0a09bdd5e306d7
Akhilesh Singh <akhilesh.singh@haud.com>
2025-01-08 19:33:23 +0530

    add more features like group management.

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 export const keycloakConfig = {
     url: 'https://nforce-haud-vm-ub22-development29.devenv.haud.report',
     realm: 'sso-poc',
+    user_management_role: 'platform_user_management',
+    group_management_role: 'platform_group_management',
     app1_client_id: 'app1',
+    app1_enabled_role: 'app1_enabled',
     app2_client_id: 'app2',
-    app3_client_id: 'app3'
+    app2_enabled_role: 'app2_enabled',
+    app3_client_id: 'app3',
+    app3_enabled_role: 'app3_enabled',
 };
